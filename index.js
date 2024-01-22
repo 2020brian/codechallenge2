3e175b24a9dc4b4295d3ddaa5a8142e8f077711b
onjorobrian <onjorobrian1@gmail.com>
2024-01-22 22:11:54 +0300

    index

---
+++ index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';  // You can create this CSS file for styling
+
+ReactDOM.render(
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
+);
