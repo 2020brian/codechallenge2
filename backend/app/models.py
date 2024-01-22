f6280730bf994f005523b6c84cbedda53c7650c8
onjorobrian <onjorobrian1@gmail.com>
2024-01-22 21:55:43 +0300

    models

---
+++ backend/app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Restaurant(db.Model):
+    # Your Restaurant model definition
+
+class Pizza(db.Model):
+    # Your Pizza model definition
+
+class RestaurantPizza(db.Model):
+    # Your RestaurantPizza model definition
