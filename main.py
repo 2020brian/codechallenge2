f8a147cb4595569c6394a58a00a8bb564c28fea5
onjorobrian <onjorobrian1@gmail.com>
2024-01-27 00:08:20 +0300

    renderdeployment

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.routes.routes import restaurant_bp, pizza_bp
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  
 
 app = Flask(__name__)
 
-# Use environment variable for database URI (update 'REPLACE_WITH_RENDER_DB_URL' with the actual environment variable name on Render)
+
 DATABASE_URI = os.environ.get('REPLACE_WITH_RENDER_DB_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 
-# Initialize SQLAlchemy and Migrate
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Register blueprints
 app.register_blueprint(restaurant_bp)
 app.register_blueprint(pizza_bp)
 
